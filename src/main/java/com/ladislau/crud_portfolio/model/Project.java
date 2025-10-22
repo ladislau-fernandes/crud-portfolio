dec79e9a0570351d54954959ac10b3a2909f72db
ladislau-fernandes <lfernandesnagy@gmail.com>
2025-10-22 12:40:42 -0300

    migrando h2 para postgresql

--- src/main/java/com/ladislau/crud_portfolio/model/Project.java
+++ src/main/java/com/ladislau/crud_portfolio/model/Project.java
@@ -1,15 +1,19 @@
 package com.ladislau.crud_portfolio.model;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name ="DB_PROJECTS")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,8 +22,11 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false, length = 200)
     private String description;
+    @Column(nullable = false)
     private String link;
 
    
